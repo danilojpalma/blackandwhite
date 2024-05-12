85ff1cc6ae0583ff9992d0c748b2ee7f375fbe54
Danilo J. Palma <dan.palmav@duocuc.cl>
2024-05-12 13:10:36 -0400

    add routes, styles and jimp logic

--- server.mjs
+++ server.mjs
@@ -1,9 +1,13 @@
 import express from 'express';
 import router from './routes/router.mjs';
+import path from 'path';
 
+const __dirname = path.resolve(); 
 
 const app = express();
 const PORT = process.env.PORT ||  3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Routes
